c68e155fd629c26011a9458073bd8721d7567798
hideoncalm <hideoncalm@gmail.com>
2021-12-28 00:43:11 +0700

    add firebase

--- app/src/main/java/com/quyen/smarthome/ui/main/MainActivity.kt
+++ app/src/main/java/com/quyen/smarthome/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.quyen.smarthome.ui.main
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.database.FirebaseDatabase
 import com.quyen.smarthome.R
 import com.quyen.smarthome.base.BaseActivity
 import com.quyen.smarthome.databinding.ActivityMainBinding
@@ -18,6 +19,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun initData() {
+        val database : FirebaseDatabase = FirebaseDatabase.getInstance("https://iot-project-ebf71-default-rtdb.asia-southeast1.firebasedatabase.app")
+        val myRef = database.getReference("name")
+        myRef.setValue("Le Ngoc Quyen dep trai vo dich")
     }
 
     private fun setUpBottomNavigation() {
